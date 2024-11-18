4ef41da66cf50e7fca0abcc042218a433238216b
arandombit <a.random.luis@proton.me>
2024-11-18 11:33:35 -0500

    integrate url shortener components & handlers
    This integrates all of the components for displaying an app that can
    accept a long url and return back a shortened url that maps to it, along
    with the copying of it to one's clipboard.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import './App.css'
+import { useState } from 'react'
+import { connect } from 'react-redux'
 
-const App = () => {
+import './App.scss'
+
+import { shortenURL } from './actions/shortenedURL'
+import { clickHandler, submitHandler } from './handlers/url'
+
+import ErrorMessage from './atoms/ErrorMessage'
+import ShortenedLink from './components/ShortenedLink'
+import ShortenerForm from './components/ShortenerForm'
+
+const App = props => {
+  const [url, setURL] = useState('')
+  const [isError, setError] = useState(false)
+  const [copySuccess, setCopySuccess] = useState(false)
+  const { shortenedURL } = props
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div className='container'>
+      <ShortenerForm
+        onSubmit={submitHandler({
+          url,
+          setURL,
+          setError,
+          shortenURL: props.shortenURL
+        })}
+        onChange={e => setURL(e.target.value)}
+        value={url} />
+      { props.shortenedURL.status === 'success' &&
+        <ShortenedLink
+          onClick={clickHandler({
+            setCopySuccess,
+            shortURL: shortenedURL.value
+          })}
+          copySuccess={copySuccess}
+          shortURL={shortenedURL.value}
+        />
+      }
+      { props.shortenedURL.status === 'fail' &&
+        <ErrorMessage message='Something went wrong on our side, try again later' /> }
+      { isError && <ErrorMessage message='Invalid URL' /> }
     </div>
   )
 }
 
-export default App
+export default connect(({ shortenedURL }) => ({ shortenedURL }), {
+  shortenURL
+})(App)
